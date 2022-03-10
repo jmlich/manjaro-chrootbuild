1d84e8289fd55d7c709b1adabc0738fd83f6ff8a
Bernhard Landauer <bernhard@manjaro.org>
2022-03-10 21:24:47 +0100

    [util-chroot] add arm keyrings

--- lib/util-chroot.sh
+++ lib/util-chroot.sh
@@ -77,16 +77,19 @@ update_chroot() {
 }
 
 create_chroot() {
+    keyrings=('archlinux' 'manjaro')
+    [[ ${ARCH} = aarch64 ]] && keyrings+=('archlinuxarm' 'manjaro-arm')
+
     create_min_fs $1
     chroot_api_mount $1 && touch $1/.{mount,lock}
     [[ ${MULTILIB} = true ]] && touch $1/.multilib
     msg "Install build environment"
     conf_pacman
-    pacman -r $1 --config ${PAC_CONF} -Syy base-devel archlinux-keyring manjaro-keyring --noconfirm || abort "Failed to install chroot filesystem."
+    pacman -r $1 --config ${PAC_CONF} -Syy base-devel "${keyrings[@]}-keyring" --noconfirm || abort "Failed to install chroot filesystem."
     [[ ${MULTILIB} = true ]] && pacman -r $1 --config ${PAC_CONF} -Syy multilib-devel --noconfirm
     msg "Populate keyrings."
     chroot $1 pacman-key --init
-    chroot $1 pacman-key --populate archlinux manjaro
+    chroot $1 pacman-key --populate "${keyrings[@]}"
 
     msg "Create locale"
     printf 'en_US.UTF-8 UTF-8\n' > "$1/etc/locale.gen"
